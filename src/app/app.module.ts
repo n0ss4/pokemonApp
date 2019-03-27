7e9febd82a80bcbf6546a302f124a4fd62fed042
Nossair <n0ss4@protonmail.com>
2019-03-27 15:41:11 +0100

    Diseño (beta)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FiltrosComponent } from './filtros/filtros.component';
+import { PokemonComponent } from './pokemon/pokemon.component';
+import { EditarComponent } from './pokemon/editar/editar.component';
+import { EliminarComponent } from './pokemon/eliminar/eliminar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FiltrosComponent,
+    PokemonComponent,
+    EditarComponent,
+    EliminarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]

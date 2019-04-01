3a4a12802fd54903850beb78b99fa36cdb80e452
divad <divad.davidam@gmail.com>
2019-04-01 15:12:26 +0200

    Aplicar filtres, el cercado, ajuntar el diseny del nossair amb el meu.

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { waitForMap } from '@angular/router/src/utils/collection';
+import { Pokemon } from '../home/pokemonObject';
+
 
 @Component({
   selector: 'app-pokemon',
@@ -6,13 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
-
-  // Numero de pokemon
-  items:number[] = new Array(30);
-
-  constructor() { }
-
-  ngOnInit() {
+  @Input() arrayPokemon: Pokemon[];
+  @Input('verEstadistica') verEstadistica: boolean;
+  @Input('filterPost') filterPost: string;
+   constructor() { }
+    ngOnInit() {
+    this.verEstadistica = true;
+  
   }
-
+ 
 }

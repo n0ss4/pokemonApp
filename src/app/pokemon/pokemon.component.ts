228e1ea0263db2d91d63936084b317cf8faa67da
divad <divad.davidam@gmail.com>
2019-04-04 15:19:06 +0200

    Subir modal

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -2,12 +2,15 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Pokemon } from '../home/pokemonObject';
 import {HttpClient} from '@angular/common/http';
 import { EditarComponent } from './editar/editar.component';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
   selector: 'app-pokemon',
   templateUrl: './pokemon.component.html',
-  styleUrls: ['./pokemon.component.css']
+  styleUrls: ['./pokemon.component.css'],
+  providers: [NgbModalConfig, NgbModal]
+
 })
 export class PokemonComponent implements OnInit {
 
@@ -16,7 +19,13 @@ export class PokemonComponent implements OnInit {
   @Input('filterPost') filterPost: string;
   @ViewChild(EditarComponent) modal: EditarComponent;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,config: NgbModalConfig, private modalService: NgbModal) { 
+    config.backdrop = 'static';
+    config.keyboard = false;
+  }
+  open(content) {
+    this.modalService.open(content);
+  }
 
   ngOnInit() {
     this.verEstadistica = true;
